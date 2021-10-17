eaf9e6c8fc6b2648f2b644dcace07fe5d657c087
Andrey Tsaryk <tsaryk1323an@gmail.com>
2021-10-18 01:51:59 +0300

    Creating UI app

---
+++ app/src/main/java/com/tsaryk/test_prject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tsaryk.test_prject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
