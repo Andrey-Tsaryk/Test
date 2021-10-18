8f4b59d9e5691c8da69b4a66ee35bbf60cf2bc5c
Andrey Tsaryk <tsaryk1323an@gmail.com>
2021-10-18 05:02:21 +0300

    ui

--- app/src/main/java/com/tsaryk/test_prject/MainActivity.java
+++ app/src/main/java/com/tsaryk/test_prject/MainActivity.java
@@ -3,12 +3,31 @@ package com.tsaryk.test_prject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity<Firebase> extends AppCompatActivity {
+
+    private EditText edit_t;
+    Firebase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = findViewById(R.id.editText);
+        edit_t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Edit_t = edit_t.getText().toString();
+                List<String> edit_t = List<>();
+                edit_t.put(edit_t);
+
+                db.collection();
+            }
+        });
     }
 }
